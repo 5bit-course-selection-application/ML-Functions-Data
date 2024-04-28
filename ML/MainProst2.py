abbec1a3f82214f80662d5fe5e3434d95dc99ff0
Chepko Daniil <chepko_daniil@mail.ru>
2024-04-28 10:04:44 +0300

    new models

--- ML/MainProst2.py
+++ ML/MainProst2.py
@@ -1,57 +1,57 @@
-import pandas as pd
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import json
+from scipy.sparse.linalg import svds
+import numpy as np
 
-def load_data(filepath):
-    try:
-        return pd.read_csv(filepath)
-    except Exception as e:
-        print(f"Error loading data: {e}")
-        return pd.DataFrame()
 
-def preprocess_data(dataframe):
-    dataframe['Keys'] = dataframe['Keys'].apply(eval)
-    return dataframe
+def process_data(courses, vacancy):
+    """Подготовка данных для векторизации"""
+    courses_desc = [" ".join([course.get('desc', '')] + course.get('keywords', [])) for course in courses]
+    vacancy_keys = " ".join([vacancy.get('Description', '')] + vacancy.get('Keys', []))
+    return courses_desc, [vacancy_keys]  
 
-def get_course_recommendations(vacancy_data, courses_df, top_n=3):
-    vacancy_data['Keywords'] = vacancy_data['Keys'].apply(lambda x: ' '.join(x))
+def calculate_similarity(course_desc, vacancy_keys):
+    """Расчет сходства"""
+    vectorizer = TfidfVectorizer()
+    all_texts = vacancy_keys + course_desc
+    tfidf_matrix = vectorizer.fit_transform(all_texts)
     
-    tfidf_vectorizer = TfidfVectorizer()
-    all_texts = vacancy_data['Keywords'].tolist() + courses_df['description'].tolist()
-    tfidf_matrix = tfidf_vectorizer.fit_transform(all_texts)
+    k = min(tfidf_matrix.shape) - 1
     
-    vacancy_vectors = tfidf_matrix[:len(vacancy_data)]
-    course_vectors = tfidf_matrix[len(vacancy_data):]
+    u, s, vt = svds(tfidf_matrix, k=k)
+    s_matrix = np.diag(s)
+    reduced_tfidf = np.dot(u, s_matrix)
     
-    similarity_scores = cosine_similarity(vacancy_vectors, course_vectors)
+    vacancy_vectors = reduced_tfidf[:len(vacancy_keys)]
+    course_vectors = reduced_tfidf[len(vacancy_keys):]
     
-    recommendations = []
-    for idx, vacancy in enumerate(vacancy_data.itertuples()):
-        vacancy_recommendations = {
-            "vacancy_id": vacancy.Ids,
-            "vacancy_name": vacancy.Name,
-            "recommended_courses": []
-        }
-        course_sim_scores = similarity_scores[idx]
-        top_courses = sorted(list(enumerate(course_sim_scores)), key=lambda x: x[1], reverse=True)[:top_n]
-        
-        for course_idx, score in top_courses:
-            course_info = {
-                "course_name": courses_df.iloc[course_idx]['name'],
-                "course_description": courses_df.iloc[course_idx]['description'],
-                "similarity_score": score
-            }
-            vacancy_recommendations["recommended_courses"].append(course_info)
-        
-        recommendations.append(vacancy_recommendations)
-    
+    return cosine_similarity(vacancy_vectors, course_vectors)
+
+def get_recommendations(courses, vacancy, top_n=3):
+    """Генерация рекомендаций на основе расчетного сходства"""
+    course_desc, vacancy_keys = process_data(courses, vacancy)
+    similarity_scores = calculate_similarity(course_desc, vacancy_keys)
+    course_indices = np.argsort(similarity_scores[0])[::-1][:top_n]  
+    recommendations = {
+        'vacancy_id': vacancy.get('Ids', ''),
+        'vacancy_name': vacancy.get('Name', ''),
+        'recommendations': [
+            {
+                'course_id': courses[i].get('_id', ''),
+                'course_name': courses[i].get('name', ''),
+                'course_description': courses[i].get('desc', ''),
+                'course_rating': courses[i].get('rating', 'Not rated'),
+                'similarity_score': similarity_scores[0][i]
+            } for i in course_indices
+        ]
+    }
     return recommendations
 
-vacancies = load_data('path_to_vacancies.csv')
-vacancies = preprocess_data(vacancies)
-courses_df = load_data('path_to_courses.csv')  # файл с курсами
+# courses_data = [...]  # Список словарей с информацией о курсах
+# vacancy_data = {...}  # Одиночный словарь с информацией о вакансии
+# recommendations = get_recommendations(courses_data, vacancy_data, top_n=3)
 
-recommendations = get_course_recommendations(vacancies, courses_df)
-json_output = json.dumps(recommendations, indent=4)
-print(json_output)
+
+
+
